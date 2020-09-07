f37da1d0ca93f7a53ff18165aae4171fb27d47ab
Diego Sandoval <sandovdiego@gmail.com>
2020-09-07 01:08:50 -0300

    fix indentation and change envs directory

--- em.bash
+++ em.bash
@@ -1,30 +1,32 @@
 #APPEND THIS CODE TO YOUR .bashrc file
+#CREATE THE DIRECTORY ~/.python_envs/
 
 em(){
 	if [[ -z $1 ]]; then
 		echo "subcommands: ls, mk, rm, a, d";
 	elif [[ $1 == "ls" ]]; then 
-		ls ~/Documents/apps/python_envs/;
+		ls ~/.python_envs/;
 
 	elif [[ $1 == "mk" && -z $2 ]]; then
 		echo "can't make: no name specified";
-    elif [[ $1 == "mk" ]]; then
-        #echo "making environment ${2}";
-        virtualenv ~/Documents/apps/python_envs/${2};
+	elif [[ $1 == "mk" ]]; then
+        	#echo "making environment ${2}";
+        	virtualenv ~/.python_envs/${2};
+	
+	elif [[ $1 == "rm" && -z $2 ]]; then
+		echo "can't remove: no name specified";
+	elif [[ $1 == "rm" ]]; then
+		rm -rf ~/.python_envs/${2}/;
 
-    elif [[ $1 == "rm" && -z $2 ]]; then
-        echo "can't remove: no name specified";
-    elif [[ $1 == "rm" ]]; then
-        rm -rf ~/Documents/apps/python_envs/${2}/;
+	elif [[ $1 == "a" && -z $2 ]]; then
+		echo "can't activate: no name specified";
+	elif [[ $1 == "a" ]]; then
+		#echo "activating environment ${2}";
+		source ~/.python_envs/${2}/bin/activate;
 
-    elif [[ $1 == "a" && -z $2 ]]; then
-        echo "can't activate: no name specified";
-    elif [[ $1 == "a" ]]; then
-        #echo "activating environment ${2}";
-        source ~/Documents/apps/python_envs/${2}/bin/activate;
-
-    elif [[ $1 == "d" ]]; then
-        #echo "deactivating environment";
-        deactivate;
+	elif [[ $1 == "d" ]]; then
+		#echo "deactivating environment";
+		deactivate;
 	fi
 }
+
